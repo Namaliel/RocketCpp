07e0e822d0eebf040aa837edf8baad25113b67d3
Namaliel <mr.sunshinedaylight@outlook.com>
2021-03-16 21:11:55 -0600

    Progreso

--- src/Formulas.cpp
+++ src/Formulas.cpp
@@ -3,4 +3,26 @@
 #include <vector>
 #include <iostream>
 
+double RawRise(double mass, double height)
+{
+    double g = 9.81;
+    double result = g*mass*height;
+    return result;
+}
 
+double Gravity(double earth, double height)
+{
+    double result;
+    double constant = 6.67430e-11;
+    temp = earth / pow(height,2);
+    result = temp * constant;
+    return result;
+}
+
+class Object
+{
+    public:
+        std::vector<double> x;
+        std::vector<double> y;
+        std::vector<double> z;
+}
